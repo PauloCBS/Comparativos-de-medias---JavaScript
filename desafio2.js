e06e50ed6d094f5e36a9dd3172ea063472cae7a5
Your Name <paulo-cesar-batista@hotmail.com>
2022-03-02 17:42:15 +0000

    Desafio dois em criar um programa que recebe uma nota e verifica se o aluno esta dentro da media atraves do uso de condicionais de comparacao.

---
+++ desafio2.js
@@ -0,0 +1,11 @@
+
+var media = 7; // nota media
+var nota = prompt('digite a nota do aluno'); //janela de prompt para inserir a nota.
+
+if(nota >= media ){ //se a nota for maior ou igual a media.
+    document.write('O aluno foi Aprovado'); // escrever o aluno foi aprovado.
+}else{ // caso contratrio escrever o aluno foi reprovado.
+    document.write('O aluno foi reprovado');
+}
+
+// as observacoes seriam se o dado for string ou int.
